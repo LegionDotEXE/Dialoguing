c9f900a5d0abc8ff24128494aab59b4b11a8c183
Saurab S <ajay.v.v.743@gmail.com>
2026-02-18 20:14:07 -0800

    Initial

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// Saurav Shah
+// Date: 02/18/2026
+
+// Dialoguing
+
+
+// TO-DO: integrate the end-of-line fix from ourcade: https://blog.ourcade.co/posts/2020/phaser-3-typewriter-text-effect-bitmap/
+
+// BE STRIK
+"use strict"
+
+// game config
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: [ Title, Talking ]
+}
+
+const game = new Phaser.Game(config)
+
+// globals
+const centerX = game.config.width / 2
+const centerY = game.config.height / 2
+let cursors = null
